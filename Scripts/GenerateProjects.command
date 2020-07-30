e2a675a273e3ee53a5793b0e20e731a35740b1f7
SparkyPotato <noob.sparkypotato@gmail.com>
2020-07-30 21:38:31 +0530

    Added Mac building

---
+++ Scripts/GenerateProjects.command
@@ -0,0 +1,4 @@
+pushd ../
+ThirdParty/Premake/premake5-mac xcode4
+popd
+
--- Vortex/Source/Core/Core.h
+++ Vortex/Source/Core/Core.h
@@ -6,6 +6,14 @@
 	#else
 		#error "Vortex is 64-bit only!"
 	#endif
+#elif
+    #ifdef __APPLE__
+        #include <TargetConditionals.h>
+        #if TARGET_PLATFORM_OSX == 1
+            #define PLATFORM_MAC
+        #else
+            #error "macOS is the only Apple OS supported!"
+        #endif
 #else
 	#error "Windows is the only supported platform as of yet!"
 #endif
