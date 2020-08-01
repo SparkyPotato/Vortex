c5503d9f8e8ac8c9b13a26c9a5a3a1437503f8a4
Sparky <noob.sparkypotato@gmail.com>
2020-08-01 14:46:51 +0530

    Actually created a constant buffer in the create method

--- Vortex/Source/Private/Graphics/GPConstantBuffer.cpp
+++ Vortex/Source/Private/Graphics/GPConstantBuffer.cpp
@@ -1,10 +1,19 @@
 #include <VXpch.h>
 #include <Graphics/Primitives/GPConstantBuffer.h>
+#ifdef PLATFORM_WINDOWS
+#include <Private/Platforms/DirectX11/DX11ConstantBuffer.h>
+#endif
 
 namespace Vortex
 {
 	GPConstantBuffer* GPConstantBuffer::Create(void* data, int size, ConstantBufferTarget target)
 	{
-
+		switch (GraphicsContext::Get()->GetAPI())
+		{
+		case GraphicsAPI::DirectX11:
+			return new DX11ConstantBuffer(data, size, target);
+		default:
+			break;
+		}
 	}
 }
